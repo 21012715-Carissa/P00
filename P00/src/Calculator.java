9113d1888012b849af37304861406493a228d4b5
caris <caris@LAPTOP-4CHFTICG>
2022-05-27 13:13:59 +0800

    first push to P00 repository in Github

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -1,4 +1,24 @@
 
 public class Calculator {
-
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		if (b != 0) {
+		  return a / b;
+	}
+		else {
+			throw new IllegalArgumentException("Division by zero is not allowed."); 
+		
+      }
+   }
 }
